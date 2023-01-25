780155b09e54c0911ebd87533461894c3ee07fb9
rllyben <f.benn.mueller@gmail.com>
2023-01-25 15:37:14 +0100

    init

--- flomy/src/main.js
+++ flomy/src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import '@/assets/custom.css'
 
 createApp(App).use(router).mount('#app')
