019382214dfa26034768526299e8a25397e86aa6
Xeydra <annmarix@gmail.com>
2023-02-13 15:01:32 +0100

    dreieck fragment haha lol rofl

--- flomy/src/views/HomeView.vue
+++ flomy/src/views/HomeView.vue
@@ -1,91 +1,120 @@
 <template>
-    <div class="main">
+  <div class="main">
     <nav class="navbar fixed-top">
-      <a class="navbar-brand name" href="#" tabindex="0">Meisterwerkstatt Müller</a>
+      <a class="navbar-brand name" href="#" tabindex="0"
+        >Meisterwerkstatt Müller</a
+      >
     </nav>
     <div class="bannercontainer">
-      <img class="ecke" src="@/assets/eckenbanner.svg">
-      <img class="banner" src="@/assets/engine.jpg">
-      <img class="logo" src="@/assets/logo.svg">
+      <img class="ecke" src="@/assets/eckenbanner.svg" />
+      <img class="banner" src="@/assets/engine.jpg" />
+      <img class="logo" src="@/assets/logo.svg" />
     </div>
     <div class="container blop">
       <div class="row">
         <div class="halt">
-          <h1> About Us </h1>
+          <h1>About Us</h1>
         </div>
-      <div class="row halt">
-        <p> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-        </p>
-      </div>
-      <div class="row">
-        <div class="col">
-          <p1 class="left">  Tel. 06128 84322 <br>
-                Mail: info@kfz-mwm.de </p1>
+        <div class="row halt">
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+            et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+            no sea takimata sanctus est Lorem ipsum dolor sit amet.
+          </p>
         </div>
-        <div class="col">
-          <p1>  Mo: 08:00 - 13:00 Uhr <br>
-               Fr:  14:00 - 18:00 Uhr </p1>
+        <div class="row">
+          <div class="col">
+            <p1 class="left">
+              Tel. 06128 84322 <br />
+              Mail: info@kfz-mwm.de
+            </p1>
+          </div>
+          <div class="col">
+            <p1>
+              Mo: 08:00 - 13:00 Uhr <br />
+              Fr: 14:00 - 18:00 Uhr
+            </p1>
+          </div>
+          <div class="col">
+            <p1>
+              Sa: geschlossen <br />
+              So: geschlossen
+            </p1>
+          </div>
+          <div class="col">
+            <p class="right">
+              Obere Waldstraße 9 <br />
+              65232 Taunusstein <br />
+              Deutschland
+            </p>
+          </div>
         </div>
-        <div class="col">
-          <p1>   Sa:  geschlossen <br>
-                So: geschlossen </p1>
-        </div>
-        <div class="col">
-          <p class="right">  Obere Waldstraße 9 <br>
-                65232 Taunusstein <br>
-                Deutschland </p>
-        </div>
-      </div>
       </div>
     </div>
     <div class="container blop">
       <div class="halt">
-        <h1> Unser Service </h1>
+        <h1>Unser Service</h1>
       </div>
       <div class="row">
         <div class="row halt">
-          <p> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+            et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+            no sea takimata sanctus est Lorem ipsum dolor sit amet.
           </p>
         </div>
-        <!-- <div class="background">
-          <img class="banner" src="@/assets/footer.svg">
-        </div> -->
-        <div class="row">
-          <div class="col service camper" style="text-align: center">
-            <h3> Wohnmobile </h3>
-          </div>
-          <div class="col service car" style="text-align: center">
-            <h3> Kraftfahrzeuge </h3>
-          </div>
-          <div class="col service e-car" style="text-align: center">
-            <h3> E-Autos </h3>
-          </div>
+        <div class="background">
+          <img class="fragment" src="@/assets/footer.svg" />
         </div>
-      </div>
-      </div>
-    <div class="container">
-      <div class="row">
-        <div class="col flexbox">
-          <button type="button" class="kontaktformular"> <h4>Kontaktformular</h4></button>
+        <div class="custom-row">
+          <div class="col service camper">
+            <h3>Wohnmobile</h3>
+          </div>
+          <div class="col service car">
+            <h3>Kraftfahrzeuge</h3>
+          </div>
+          <div class="col service e-car">
+            <h3>E-Autos</h3>
+          </div>
         </div>
       </div>
     </div>
-    <footer>
-      <div class="container impressum">
-          Impressum
+    <div class="container">
+      <div class="row">
+        <div class="col flexbox">
+          <button type="button" class="kontaktformular">
+            <h4>Kontaktformular</h4>
+          </button>
+        </div>
       </div>
+    </div>
+
+    <footer>
+      <div class="container impressum">Impressum</div>
     </footer>
   </div>
 </template>
 
 
 <style>
-.main{
+.main {
   position: relative;
 }
-.bannercontainer{
+.bannercontainer {
   display: grid;
-  grid-template-areas: 'overlay';
+  grid-template-areas: "overlay";
 }
 .bannercontainer img {
   grid-area: overlay;
@@ -99,7 +128,8 @@
 .logo {
   z-index: 2;
   place-self: end;
-  margin-right: 4%;
+  margin-right: 5%;
+  width: 20%;
 }
 .ecke {
   width: 100%;
@@ -107,13 +137,13 @@
   z-index: 1;
 }
 nav {
-  background-color: #2D2D2D;
+  background-color: #2d2d2d;
 }
 .name {
   padding-left: 50px;
 }
 .name:hover {
-  color: #FFC678;
+  color: #ffc678;
 }
 .right {
   text-align: right;
@@ -138,24 +168,22 @@ hr {
   color: var(--main-text-color);
   padding: 40px;
   padding-bottom: 25vmax;
-  margin: 2rem;
+  text-align: center;
 }
 .camper {
   background-color: #585979;
-  margin-left: -1.5rem;
 }
 .car {
   background-color: #517350;
 }
 .e-car {
-  background-color: #6D5151;
-  margin-right: -3rem;
+  background-color: #6d5151;
 }
 .flexbox {
   display: flex;
 }
 .kontaktformular {
-  background-color: #2D2D2D;
+  background-color: #2d2d2d;
   color: var(--main-text-color);
   height: 65px;
   width: 280px;
@@ -164,12 +192,22 @@ hr {
 footer {
   margin: 3rem auto;
 }
-/* .background {
+.background {
+  /* background-image: url("@/assets/footer.svg");
   background-repeat: no-repeat;
   background-position: bottom;
   position: relative;
   max-height: 0px;
   z-index: -1;
-  margin-left: -16%;
-} */
+  margin-left: -16%; */
+  position: relative;
+}
+
+.fragment {
+  z-index: -1;
+  position: absolute;
+  width: 100vw;
+  transform: translateX(-51%);
+  margin-left: calc(50%);
+}
 </style>
