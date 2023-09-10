1870795af1b8a5f9081bc6b318c642f05ddd6be2
Xeydra <annmarix@gmail.com>
2023-09-10 19:59:10 +0200

    impressum und privacy texte

--- flomy/src/views/PrivacyPageView.vue
+++ flomy/src/views/PrivacyPageView.vue
@@ -5,8 +5,152 @@
           <h1>Datenschutz</h1>
         </div>
         <div class="row">
+          <h2>Verantwortlicher für die Datenverarbeitung auf dieser Webseite:</h2>
           <p>
-            Wir nehmen ihre Daten und verkaufen sie an den höchst bietenden (meist Google oder Facebook und das für viel Money *-*)
+            Hans-Peter Müller <br />
+            Obere Waldstraße 9 <br />
+            65232 Taunusstein <br />
+            Telefon: +49 (0) 6128 84322 <br />
+            E-Mail: info@kfz-mwm.de
+          </p>
+
+          <h2>Allgemeine Hinweise</h2>
+          <p>
+            In den nachfolgenden Hinweisen erhalten Sie eine einfache Übersicht darüber, wie wir Ihre persönlichen Informationen behandeln, wenn Sie unsere Website besuchen. Personenbezogene Daten sind jegliche Informationen, die dazu verwendet werden können, Sie persönlich zu identifizieren. Für ausführlichere Informationen zum Thema Datenschutz verweisen wir auf unsere, hierauf folgende, Datenschutzerklärung.
+            <br />
+            Wir möchten darauf hinweisen, dass die Datenübertragung im Internet, beispielsweise bei der Kommunikation per E-Mail, Sicherheitsrisiken aufweisen kann. Ein absoluter Schutz Ihrer Daten vor dem Zugriff durch Dritte ist nicht gewährleistet.
+          </p>
+          <h2>          
+            Wer trägt die Verantwortung für die Datenerhebung auf dieser Webseite?
+          </h2>
+          <p>
+            Die Verarbeitung von Daten auf dieser Webseite wird durch den Betreiber der Webseite durchgeführt. Die Kontaktinformationen des Betreibers sind im Impressum dieser Webseite aufgeführt.
+          </p>
+          <h2>
+            Wie erfolgt die Datenerhebung?
+          </h2>
+          <p>
+            Ihre Daten werden auf verschiedene Weisen erfasst. Eine Möglichkeit besteht darin, dass Sie uns Ihre Daten aktiv mitteilen, beispielsweise durch die Eingabe von Informationen in ein Kontaktformular. Andererseits werden bei Ihrem Besuch auf unserer Webseite automatisch bestimmte Daten von unseren IT-Systemen erfasst. Hierbei handelt es sich insbesondere um technische Informationen wie den verwendeten Internetbrowser, das Betriebssystem und die Zeit des Seitenaufrufs. Die Erfassung dieser Daten erfolgt automatisch, sobald Sie unsere Webseite betreten.
+          </p>
+          <h2>
+            Zu welchem Zweck verwenden wir Ihre Daten?
+          </h2>
+          <p>
+            Ein Teil der Daten wird gesammelt, um sicherzustellen, dass die Website einwandfrei funktioniert. Andere Informationen können zur Analyse Ihres Nutzerverhaltens genutzt werden.
+          </p>
+          <h2>
+            Analyse-Tools und Tools von Drittanbietern
+          </h2>
+          <p>
+            Während Ihres Besuchs auf unserer Website kann Ihr Surfverhalten statistisch ausgewertet werden, in der Regel mithilfe von Cookies und Analyseprogrammen. Diese Analyse erfolgt in der Regel anonym und lässt keine Rückschlüsse auf Ihre Person zu. Sie haben die Möglichkeit, dieser Analyse zu widersprechen oder sie zu verhindern, indem Sie bestimmte Tools nicht verwenden. Weitere Informationen dazu finden Sie in unserer ausführlichen Datenschutzerklärung. Dort werden auch die Möglichkeiten zum Widerspruch gegen diese Analyse erläutert.
+          </p>
+          <h2>
+            Widerruf Ihrer Einwilligung zur Datenverarbeitung
+          </h2>
+          <p>
+            Viele Datenverarbeitungsvorgänge sind nur mit Ihrer ausdrücklichen Einwilligung möglich. Sie können eine bereits erteilte Einwilligung jederzeit widerrufen. Dazu reicht eine formlose Mitteilung per E-Mail an uns. Die Rechtmäßigkeit der bis zum Widerruf erfolgten Datenverarbeitung bleibt vom Widerruf unberührt.
+            <br />
+            Sie haben jederzeit das Recht, aus Gründen, die sich aus Ihrer besonderen Situation ergeben, gegen die Verarbeitung Ihrer personenbezogenen Daten Widerspruch einzulegen; dies gilt auch für ein auf diese Bestimmungen gestütztes Profiling. Wenn Sie Widerspruch einlegen, werden wir Ihre betroffenen personenbezogenen Daten nicht mehr verarbeiten, es sei denn, wir können zwingende schutzwürdige Gründe für die Verarbeitung nachweisen, die Ihre Interessen, Rechte und Freiheiten überwiegen oder die Verarbeitung dient der Geltendmachung, Ausübung oder Verteidigung von Rechtsansprüchen (Widerspruch nach Art. 21 Abs. 1 DSGVO).
+            <br />
+            Werden Ihre personenbezogenen Daten verarbeitet, um Direktwerbung zu betreiben, so haben Sie das Recht, jederzeit Widerspruch gegen die Verarbeitung Sie betreffender personenbezogener Daten zum Zwecke derartiger Werbung einzulegen; dies gilt auch für das Profiling, soweit es mit solcher Direktwerbung in Verbindung steht. Wenn Sie widersprechen, werden Ihre personenbezogenen Daten anschließend nicht mehr zum Zwecke der Direktwerbung verwendet (Widerspruch nach Art. 21 Abs. 2 DSGVO).
+          </p>
+          <h2>
+            Beschwerderecht bei der zuständigen Aufsichtsbehörde
+          </h2>
+          <p>
+            Im Falle datenschutzrechtlicher Verstöße steht dem Betroffenen ein Beschwerderecht bei der zuständigen Aufsichtsbehörde zu. Zuständige Aufsichtsbehörde in datenschutzrechtlichen Fragen ist der Landesdatenschutzbeauftragte des Bundeslandes, in dem unser Unternehmen seinen Sitz hat. Eine Liste der Datenschutzbeauftragten sowie deren Kontaktdaten können folgendem Link entnommen werden:https://www.bfdi.bund.de/DE/Infothek/Anschriften_Links/anschriften_links-node.html.
+          </p>
+          <h2>
+            Auskunft, Sperrung, Löschung und Berichtigung
+          </h2>
+          <p>
+            Sie haben das Recht, jederzeit kostenfrei Informationen über die Herkunft, den Empfänger und den Zweck Ihrer gespeicherten persönlichen Daten zu erhalten. Ebenfalls steht Ihnen das Recht zu, die Berichtigung, Sperrung oder Löschung dieser Daten zu verlangen. Bei Fragen zum Datenschutz oder zur Ausübung Ihrer Rechte können Sie uns jederzeit unter der im Impressum angegebenen Adresse kontaktieren. Darüber hinaus haben Sie das Recht, unter bestimmten Bedingungen die Beschränkung der Verarbeitung Ihrer persönlichen Daten zu beantragen.
+          </p>
+          <h2>
+            Recht auf Datenübertragbarkeit
+          </h2>
+          <p>
+            Sie haben das Recht, Daten, die wir auf Grundlage Ihrer Einwilligung oder in Erfüllung eines Vertrags automatisiert verarbeiten, an sich oder an einen Dritten in einem gängigen, maschinenlesbaren Format aushändigen zu lassen. Sofern Sie die direkte Übertragung der Daten an einen anderen Verantwortlichen verlangen, erfolgt dies nur, soweit es technisch machbar ist.
+          </p>
+          <h2>
+            Recht auf Einschränkung der Verarbeitung
+          </h2>
+          <p>
+            Sie haben das Recht, die Einschränkung der Verarbeitung Ihrer personenbezogenen Daten zu verlangen. Hierzu können Sie sich jederzeit unter der im Impressum angegebenen Adresse an uns wenden. Das Recht auf Einschränkung der Verarbeitung besteht in folgenden Fällen:
+            <br />
+            <ul>
+              <li>
+                Wenn Sie die Richtigkeit Ihrer bei uns gespeicherten personenbezogenen Daten bestreiten, benötigen wir in der Regel Zeit, um dies zu überprüfen. Für die Dauer der Prüfung haben Sie das Recht, die Einschränkung der Verarbeitung Ihrer personenbezogenen Daten zu verlangen.
+              </li>
+              <li>
+                Wenn die Verarbeitung Ihrer personenbezogenen Daten unrechtmäßig geschah / geschieht, können Sie statt der Löschung die Einschränkung der Datenverarbeitung verlangen.
+              </li>
+              <li>
+                Wenn wir Ihre personenbezogenen Daten nicht mehr benötigen, Sie sie jedoch zur Ausübung, Verteidigung oder Geltendmachung von Rechtsansprüchen benötigen, haben Sie das Recht, statt der Löschung die Einschränkung der Verarbeitung Ihrer personenbezogenen Daten zu verlangen.
+              </li>
+              <li>
+                Wenn Sie einen Widerspruch nach Art. 21 Abs. 1 DSGVO eingelegt haben, muss eine Abwägung zwischen Ihren und unseren Interessen vorgenommen werden. Solange noch nicht feststeht, wessen Interessen überwiegen, haben Sie das Recht, die Einschränkung der Verarbeitung Ihrer personenbezogenen Daten zu verlangen.
+              </li>
+              <li>
+                Wenn Sie die Verarbeitung Ihrer personenbezogenen Daten eingeschränkt haben, dürfen diese Daten – von ihrer Speicherung abgesehen – nur mit Ihrer Einwilligung oder zur Geltendmachung, Ausübung oder Verteidigung von Rechtsansprüchen oder zum Schutz der Rechte einer anderen natürlichen oder juristischen Person oder aus Gründen eines wichtigen öffentlichen Interesses der Europäischen Union oder eines Mitgliedstaats verarbeitet werden.
+              </li>
+            </ul>
+          </p>
+          <h2>
+            Widerspruch gegen Werbe-Mails
+          </h2>
+          <p>
+            Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten zur Übersendung von nicht ausdrücklich angeforderter Werbung und Informationsmaterialien wird hiermit widersprochen. Die Betreiber der Seiten behalten sich ausdrücklich rechtliche Schritte im Falle der unverlangten Zusendung von Werbeinformationen, etwa durch Spam-E-Mails, vor.
+          </p>
+          <h1>
+            Datenerfassung auf unserer Website
+          </h1>
+          <h2>
+            Cookies
+          </h2>
+          <p>
+            Die Internetseiten verwenden teilweise so genannte Cookies. Cookies richten auf Ihrem Rechner keinen Schaden an und enthalten keine Viren. Cookies dienen dazu, unser Angebot nutzerfreundlicher, effektiver und sicherer zu machen. Cookies sind kleine Textdateien, die auf Ihrem Rechner abgelegt werden und die Ihr Browser speichert.
+            <br />
+            Die meisten der von uns verwendeten Cookies sind so genannte “Session-Cookies”. Sie werden nach Ende Ihres Besuchs automatisch gelöscht. Andere Cookies bleiben auf Ihrem Endgerät gespeichert bis Sie diese löschen. Diese Cookies ermöglichen es uns, Ihren Browser beim nächsten Besuch wiederzuerkennen.
+            Sie können Ihren Browser so einstellen, dass Sie über das Setzen von Cookies informiert werden und Cookies nur im Einzelfall erlauben, die Annahme von Cookies für bestimmte Fälle oder generell ausschließen sowie das automatische Löschen der Cookies beim Schließen des Browser aktivieren. Bei der Deaktivierung von Cookies kann die Funktionalität dieser Website eingeschränkt sein.
+            <br />
+            Cookies, die zur Durchführung des elektronischen Kommunikationsvorgangs oder zur Bereitstellung bestimmter, von Ihnen erwünschter Funktionen (z.B. Warenkorbfunktion) erforderlich sind, werden auf Grundlage von Art. 6 Abs. 1 lit. f DSGVO gespeichert. Der Websitebetreiber hat ein berechtigtes Interesse an der Speicherung von Cookies zur technisch fehlerfreien und optimierten Bereitstellung seiner Dienste. Soweit andere Cookies (z.B. Cookies zur Analyse Ihres Surfverhaltens) gespeichert werden, werden diese in dieser Datenschutzerklärung gesondert behandelt.
+          </p>
+          <h2>
+            Server-Log-Dateien
+          </h2>
+          <p>
+            Der Provider der Seiten erhebt und speichert automatisch Informationen in so genannten Server-Log-Dateien, die Ihr Browser automatisch an uns übermittelt. Dies sind:
+            <ul>
+                Browsertyp und Browserversion
+                verwendetes Betriebssystem
+                Referrer URL
+                Hostname des zugreifenden Rechners
+                Uhrzeit der Serveranfrage
+                IP-Adresse
+              </ul>
+              Eine Zusammenführung dieser Daten mit anderen Datenquellen wird nicht vorgenommen.
+              <br />
+              Grundlage für die Datenverarbeitung ist Art. 6 Abs. 1 lit. f DSGVO, der die Verarbeitung von Daten zur Erfüllung eines Vertrags oder vorvertraglicher Maßnahmen gestattet.
+          </p>
+
+          <h2>
+            Kontaktformular
+          </h2>
+          <p>
+            Wenn Sie uns per Kontaktformular Anfragen zukommen lassen, werden Ihre Angaben aus dem Anfrageformular inklusive der von Ihnen dort angegebenen Kontaktdaten zwecks Bearbeitung der Anfrage und für den Fall von Anschlussfragen bei uns gespeichert. Diese Daten geben wir nicht ohne Ihre Einwilligung weiter.
+            <br />
+            Die Verarbeitung der in das Kontaktformular eingegebenen Daten erfolgt somit ausschließlich auf Grundlage Ihrer Einwilligung (Art. 6 Abs. 1 lit. a DSGVO). Sie können diese Einwilligung jederzeit widerrufen. Dazu reicht eine formlose Mitteilung per E-Mail an uns. Die Rechtmäßigkeit der bis zum Widerruf erfolgten Datenverarbeitungsvorgänge bleibt vom Widerruf unberührt.
+            <br />
+            Die von Ihnen im Kontaktformular eingegebenen Daten verbleiben bei uns, bis Sie uns zur Löschung auffordern, Ihre Einwilligung zur Speicherung widerrufen oder der Zweck für die Datenspeicherung entfällt (z.B. nach abgeschlossener Bearbeitung Ihrer Anfrage). Zwingende gesetzliche Bestimmungen – insbesondere Aufbewahrungsfristen – bleiben unberührt.
+          </p>
+          <h2>
+            Anfrage per E-Mail, Telefon oder Telefax
+          </h2>
+          <p>
+            Wenn Sie uns per E-Mail, Telefon oder Telefax kontaktieren, wird Ihre Anfrage inklusive aller daraus hervorgehenden personenbezogenen Daten (Name, Anfrage) zum Zwecke der Bearbeitung Ihres Anliegens bei uns gespeichert und verarbeitet. Diese Daten geben wir nicht ohne Ihre Einwilligung weiter.
+            <br />
+            Die Verarbeitung dieser Daten erfolgt auf Grundlage von Art. 6 Abs. 1 lit. b DSGVO, sofern Ihre Anfrage mit der Erfüllung eines Vertrags zusammenhängt oder zur Durchführung vorvertraglicher Maßnahmen erforderlich ist. In allen übrigen Fällen beruht die Verarbeitung auf Ihrer Einwilligung (Art. 6 Abs. 1 lit. a DSGVO) und / oder auf unseren berechtigten Interessen (Art. 6 Abs. 1 lit. f DSGVO), da wir ein berechtigtes Interesse an der effektiven Bearbeitung der an uns gerichteten Anfragen haben. Die von Ihnen an uns per Kontaktanfragen übersandten Daten verbleiben bei uns, bis Sie uns zur Löschung auffordern, Ihre Einwilligung zur Speicherung widerrufen oder der Zweck für die Datenspeicherung entfällt (z. B. nach abgeschlossener Bearbeitung Ihres Anliegens). Zwingende gesetzliche Bestimmungen – insbesondere gesetzliche Aufbewahrungsfristen – bleiben unberührt.
           </p>
         </div>
       </div>
@@ -14,18 +158,18 @@
 </template>
 
 <style>
-h4{
+h4 {
   text-align: center;
 }
-.bold{
+.bold {
   font-weight: 750;
 }
 </style>
 
 <script>
-  export default {
-    mounted () {
-      window.scrollTo(0, 0)
-    }
-  }
+export default {
+  mounted() {
+    window.scrollTo(0, 0);
+  },
+};
 </script>
