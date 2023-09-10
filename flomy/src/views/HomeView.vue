29c390780f8239d20dcc9860230ee164d6f139d8
Xeydra <annmarix@gmail.com>
2023-09-10 20:20:55 +0200

    über uns text

--- flomy/src/views/HomeView.vue
+++ flomy/src/views/HomeView.vue
@@ -5,39 +5,49 @@
         <h1>Unser Service</h1>
       </div>
       <div class="row">
-<!--         <div class="background">
+        <!--         <div class="background">
           <img class="fragment" src="@/assets/footer.svg" />
         </div> -->
         <div class="custom-row boxcenter">
-            <div class="col service">
-              <img class="picservice" src="@/assets/Camper.jpg" />
-              <h3>Wohnmobile</h3>
-                <ul>
-                  <li>Ersatzteile</li>
-                  <li>Einrichtung</li>
-                  <li><a href=" https://katalog.movera.com/ePaper_MOVERA-Katalog-2023_EUR/#1" target="_blank">Camping Zubehör</a></li>
-                  <li><a href="https://www.reimo.com/ausbau-shop/" target="_blank">Camper & Wohnmobil Ausbau</a></li>
-                  <li>HU / AU</li>
-                  <li>Gasprüfung</li>
-                  <li>Inspektion</li>
-                  <li>Reparatur</li>
-                  <li>Wartung</li>
-                </ul>
-            </div>
-            <div class="col service">
-              <img class="picservice" src="@/assets/KFZ.jpg" />
-              <h3>Kraftfahrzeuge</h3>
-              <ul>
-                <li>Reparaturen</li>
-                <li>Achsvermessung</li>
-                <li>Lichteinstellung</li>
-                <li>Klimaservice</li>
-                <li>HU / AU</li>
-                <li>Fehlersuche</li>
-                <li>Wartungsarbeiten nach Herstellervorgaben</li>
-              </ul>
-            </div>
-           <div class="col service">
+          <div class="col service">
+            <img class="picservice" src="@/assets/Camper.jpg" />
+            <h3>Wohnmobile</h3>
+            <ul>
+              <li>Ersatzteile</li>
+              <li>Einrichtung</li>
+              <li>
+                <a
+                  href=" https://katalog.movera.com/ePaper_MOVERA-Katalog-2023_EUR/#1"
+                  target="_blank"
+                  >Camping Zubehör</a
+                >
+              </li>
+              <li>
+                <a href="https://www.reimo.com/ausbau-shop/" target="_blank"
+                  >Camper & Wohnmobil Ausbau</a
+                >
+              </li>
+              <li>HU / AU</li>
+              <li>Gasprüfung</li>
+              <li>Inspektion</li>
+              <li>Reparatur</li>
+              <li>Wartung</li>
+            </ul>
+          </div>
+          <div class="col service">
+            <img class="picservice" src="@/assets/KFZ.jpg" />
+            <h3>Kraftfahrzeuge</h3>
+            <ul>
+              <li>Reparaturen</li>
+              <li>Achsvermessung</li>
+              <li>Lichteinstellung</li>
+              <li>Klimaservice</li>
+              <li>HU / AU</li>
+              <li>Fehlersuche</li>
+              <li>Wartungsarbeiten nach Herstellervorgaben</li>
+            </ul>
+          </div>
+          <div class="col service">
             <img class="picservice" src="@/assets/eCar.jpg" />
             <h3>E-Autos</h3>
             <ul>
@@ -49,8 +59,8 @@
               <li>Fehlersuche</li>
               <li>Wartungsarbeiten nach Herstellervorgaben</li>
             </ul>
-           </div>
-           <div class="col service">
+          </div>
+          <div class="col service">
             <img class="picservice" src="@/assets/Reifen.jpg" />
             <h3>Reifen</h3>
             <ul>
@@ -62,8 +72,8 @@
               <li>Waschen</li>
               <li>Verkaufen</li>
             </ul>
-           </div>
           </div>
+        </div>
       </div>
     </div>
     <div class="container blop">
@@ -73,36 +83,41 @@
         </div>
         <div class="row halt">
           <p>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
-            et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
-            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-            no sea takimata sanctus est Lorem ipsum dolor sit amet.
+            Privat, Persönlich und im direkten Kontakt mit Ihrem KFZ-Meister:
+            Wir unterstützen Sie mit allen Anliegen rund ums Auto! Ob
+            Räderwechsel, Achsvermessung, Klimaservice oder Zahnriemenwechsel;
+            ob Mini, VW oder Audi; ob 3 oder 8,5 Tonnen.
+            <br />
+            <br />
+            Und Ihr Wohnmobil? Wissen Sie schon, wo Sie Ihre Ersatzteile
+            herbekommen? Wir unterstützen Sie gerne mit Rat, Tat, Expertenwissen
+            und Herz, denn wir restaurieren gerade selbst unser privates
+            Wohnmobil.
+            <br />
+            <br />
+            Sie finden uns in Taunusstein-Wehen direkt hinter der
+            Star-Tankstelle!
           </p>
         </div>
         <div class="row contact-about">
           <div class="col">
-              Mail: info@kfz-mwm.de<br />
-              Tel.: 06128 84322
+            Mail: info@kfz-mwm.de<br />
+            Tel.: 06128 84322
           </div>
           <div class="col">
-              Montag bis Freitag <br />
-              08:00 - 13:00 Uhr <br />
-              14:00 - 18:00 Uhr
+            Montag bis Freitag <br />
+            08:00 - 13:00 Uhr <br />
+            14:00 - 18:00 Uhr
           </div>
           <div class="col">
-              Obere Waldstraße 9 <br />
-              65232 Taunusstein <br />
-              Deutschland
+            Obere Waldstraße 9 <br />
+            65232 Taunusstein <br />
+            Deutschland
           </div>
         </div>
       </div>
     </div>
-<!--     <div class="container">
+    <!--     <div class="container">
       <div class="row">
         <div class="col flexbox">
           <router-link to="/contact" class="kontaktformular">  
@@ -112,10 +127,10 @@
       </div>
     </div> -->
     <footer>
-      <router-link to="/impressum">    
+      <router-link to="/impressum">
         <div class="container impressum">Impressum</div>
       </router-link>
-      <router-link to="/privacy">    
+      <router-link to="/privacy">
         <div class="container privacy">Datenschutz</div>
       </router-link>
     </footer>
@@ -136,7 +151,7 @@
 hr {
   margin: 30px;
 }
-.boxcenter { 
+.boxcenter {
   justify-content: center;
 }
 .service {
@@ -153,16 +168,16 @@ hr {
 .service ul {
   margin-top: 1rem;
 }
-.picservice{
+.picservice {
   top: 0;
   left: 0;
   height: 100%;
   width: 100%;
   position: absolute;
   object-fit: cover;
-  transition: 3s; 
+  transition: 3s;
 }
-.service:hover .picservice{
+.service:hover .picservice {
   top: 100%;
 }
 .flexbox {
@@ -183,8 +198,7 @@ hr {
   color: #ffc678;
 }
 footer {
-  margin-top: 5
-  rem;
+  margin-top: 5 rem;
   margin-bottom: 1rem;
 }
 .background {
