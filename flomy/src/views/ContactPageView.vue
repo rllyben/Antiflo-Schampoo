c58684c49e451495490766a64acdd4776dcbf069
rllyben <f.benn.mueller@gmail.com>
2024-09-04 10:58:05 +0200

    Erste Änderungen Kontaktformular

--- flomy/src/views/ContactPageView.vue
+++ flomy/src/views/ContactPageView.vue
@@ -3,6 +3,32 @@
       <div class="row">
         <div class="halt">
           <h1>Kontaktformular</h1>
+          <div>
+            <form>
+              <fieldset>
+                <div class="row">
+                  <div>
+                    <label>Anrede</label>
+                    <select>
+                      <option value="Herr">Herr</option>
+                      <option value="Herr">Frau</option>
+                      <option value="Herr">Divers</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="row">
+                  <div>
+                    <label>Nachname</label>
+                    <input value="">
+                  </div>
+                  <div>
+                    <label>Vorname</label>
+                    <input value="">
+                  </div>
+                </div>
+              </fieldset>
+            </form>
+          </div>
         </div>
       </div>
     </div>
